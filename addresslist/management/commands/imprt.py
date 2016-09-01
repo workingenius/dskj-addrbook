72ff769a2b417fa46f29d5214fcdd2f12361e94f
srq <srq@ktdatasoft.com>
2016-09-01 16:17:53 +0800

    flush db before import

--- addresslist/management/commands/imprt.py
+++ addresslist/management/commands/imprt.py
@@ -2,6 +2,7 @@
 
 import openpyxl
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 from addresslist.imprt import from_xlsx_worksheet
 
@@ -10,6 +11,8 @@ class Command(BaseCommand):
     help = 'import all enterprise database data from xlsx file'
 
     def handle(self, *args, **kwargs):
+        call_command('flush', interactive=False)
+
         wb = openpyxl.load_workbook(filename='./assets/SLC.xlsx', read_only=True)
         ws = wb[u'配置']
         objs = from_xlsx_worksheet(ws)
