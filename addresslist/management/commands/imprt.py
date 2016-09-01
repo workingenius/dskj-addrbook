ff4c7bee9387ac618d4ad67b0c42e4e4218be88d
srq <srq@ktdatasoft.com>
2016-09-01 16:32:21 +0800

    arguments of import command

--- addresslist/management/commands/imprt.py
+++ addresslist/management/commands/imprt.py
@@ -10,12 +10,19 @@ from addresslist.imprt import from_xlsx_worksheet
 class Command(BaseCommand):
     help = 'import all enterprise database data from xlsx file'
 
+    def add_arguments(self, parser):
+        parser.add_argument('path')
+        parser.add_argument('sheet')
+
     def handle(self, *args, **kwargs):
         call_command('flush', interactive=False)
 
-        wb = openpyxl.load_workbook(filename='./assets/SLC.xlsx', read_only=True)
-        ws = wb[u'配置']
+        wb = openpyxl.load_workbook(filename=kwargs['path'], read_only=True)
+        sheet = kwargs['sheet'].decode('utf8')
+        ws = wb[sheet]
         objs = from_xlsx_worksheet(ws)
 
         for obj in objs:
             obj.save()
+
+        self.stdout.write('done')
