13eda96ccb60998fb32f4b0bc62b241653e18068
srq <srq@ktdatasoft.com>
2016-09-04 09:51:51 +0800

    basic locaff page

--- addresslist/views.py
+++ addresslist/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import (
+    Staff, Position, Department, Contact
+)
+
+def locaff(request, id):
+    locaff = Staff.objects.get(id=id)
+    contacts = locaff.contact_set.all()
+
+    ctx = {
+        'locaff': locaff.name,
+        'contacts': contacts,
+    }
+
+    return render(request, 'addresslist/locaff.html', ctx)
+
