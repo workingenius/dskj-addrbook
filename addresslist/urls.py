6cbfd414779c16b472d972ce66168e16606567e1
srq <srq@ktdatasoft.com>
2016-09-06 17:21:51 +0800

    locaff_list_page -> main

--- addresslist/urls.py
+++ addresslist/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^locaff/(?P<id>\w+)$', views.locaff, name='locaff'),
+    url(r'^$', views.main, name='main'),
     url(r'^locaff_data/(?P<id>\w*)$', views.locaff_data, name='locaff_data'),
     url(r'^locaff/$', views.locaffs, name='locaffs'),
-    url(r'^locaff_list_page', views.locaff_list_page, name='locaff_list_page'),
 ]
--- addresslist/views.py
+++ addresslist/views.py
@@ -7,30 +7,8 @@ from .models import (
 from .options import CONTACTS
 
 
-def locaff(request, id):
-    lcf = Staff.objects.get(id=id)
-
-    depart = lcf.department_set.all()[0]
-
-    contacts = lcf.contact_set.all()
-    conts = []
-    for c in contacts:
-        conts.append({
-            'mode': CONTACTS[c.mode],
-            'value': c.value,
-        })
-
-    ctx = {
-        'locaff': lcf.name,
-        'department': depart.name,
-        'contacts': conts,
-    }
-
-    return render(request, 'addresslist/_locaff.html', ctx)
-
-
-def locaff_list_page(request):
-    return render(request, 'addresslist/_locaff.html')
+def main(request):
+    return render(request, 'addresslist/main.html')
 
 
 def locaff_data(request, id):
