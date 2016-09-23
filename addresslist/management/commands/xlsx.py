4d87a6c89f1c22c84216728786389730d7613124
srq <srq@ktdatasoft.com>
2016-09-23 10:19:06 +0800

    output to excel

---
+++ addresslist/management/commands/xlsx.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+
+from django.core.management.base import BaseCommand
+from addresslist.xlsx import output
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('id_list')
+
+    def handle(self, *args, **kwargs):
+        id_list = kwargs['id_list']
+        id_list = map(int, id_list.split(','))
+
+        output(id_list)
---
+++ addresslist/xlsx.py
@@ -0,0 +1,51 @@
+# -*- coding:utf8 -*-
+
+import openpyxl
+from .models import Staff
+
+def output(id_list):
+    staffs = Staff.objects.filter(id__in=id_list)
+    staffs = (staffs
+              .prefetch_related('contacts')
+              .prefetch_related('departments'))
+    staffs = staffs.all()
+
+    wb = openpyxl.Workbook()
+    ws1 = wb.active
+
+    ws1.append([
+        u'姓名',
+        u'部门1',
+        u'部门2',
+        u'号码',
+        u'邮件',
+        u'QQ',
+    ])
+
+    for staff in staffs:
+
+        # departs
+        depart = staff.departments.all()[0]
+        depart1 = depart.superior.name
+        depart2 = depart.name
+        if depart1 == u'北京亦庄工厂':
+            depart1 = depart2
+
+        # concats
+        contacts = staff.contacts.all()
+        cts = {}
+        for c in contacts:
+            cts[c.mode] = c.value
+
+        row = [
+            staff.name,
+            depart1,
+            depart2,
+            cts.get('PHONE'),
+            cts.get('EMAIL'),
+            cts.get('QQ'),
+        ]
+
+        ws1.append(row)
+
+    wb.save('output.xlsx')
