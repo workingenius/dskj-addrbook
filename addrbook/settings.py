725530d037b392da7bd52f92bd8397e7e0ff33da
srq <srq@ktdatasoft.com>
2016-09-26 16:30:00 +0800

    sqlite to mysql

--- addrbook/settings.py
+++ addrbook/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'addrbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': BASE_DIR + '/mysql.conf',
+        },
+        'TEST': {
+            'CHARSET': 'utf8mb4'
+        }
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
