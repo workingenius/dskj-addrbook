df9a5668bb1fcfcf22075ba2d6c6beca88818e97
srq <srq@ktdatasoft.com>
2016-10-21 14:08:54 +0800

    add login api

--- addresslist/urls.py
+++ addresslist/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^staffs$', views.LocaffList.as_view(), name='staff_list'),
     url(r'^staffs/(?P<id>\w*)$', views.LocaffDetail.as_view(), name='staff_detail'),
     url(r'^export', views.export, name='export'),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
