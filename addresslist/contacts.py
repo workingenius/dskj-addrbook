762fe41d7abfbf9140ca672d23d06a73cbcc0e17
srq <srq@ktdatasoft.com>
2016-10-31 09:29:04 +0800

    change columns in use

--- addresslist/contacts.py
+++ addresslist/contacts.py
@@ -6,13 +6,12 @@ Contact = namedtuple('Contact', ['key', 'literal'])
 
 email =     Contact(u'email',   u'电子邮件')
 phone =     Contact(u'phone',   u'直线')
-oldextnum = Contact(u'oldextnum', u'旧分机号')
-newextnum = Contact(u'newextnum', u'新分机号')
+extnum = Contact(u'extnum', u'新分机号')
 fax =       Contact(u'fax',     u'传真')
 mobile =    Contact(u'mobile',  u'手机')
 qq =        Contact(u'qq',      u'QQ')
-mac =       Contact(u'mac',     u'Mac地址')
+mac =       Contact(u'ip',     u'IP地址')
 
 contacts = [
-    newextnum, phone, fax, mobile, qq, email, mac
+    extnum, phone, fax, mobile, qq, email, mac
 ]
