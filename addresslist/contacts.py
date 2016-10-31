d330f230078f6ee7753c25ab08b67024c99db4ee
srq <srq@ktdatasoft.com>
2016-10-31 10:04:27 +0800

    fix

--- addresslist/contacts.py
+++ addresslist/contacts.py
@@ -6,12 +6,12 @@ Contact = namedtuple('Contact', ['key', 'literal'])
 
 email =     Contact(u'email',   u'电子邮件')
 phone =     Contact(u'phone',   u'直线')
-extnum = Contact(u'extnum', u'新分机号')
+extnum = Contact(u'extnum', u'分机号')
 fax =       Contact(u'fax',     u'传真')
 mobile =    Contact(u'mobile',  u'手机')
 qq =        Contact(u'qq',      u'QQ')
-mac =       Contact(u'ip',     u'IP地址')
+ip =       Contact(u'ip',     u'IP地址')
 
 contacts = [
-    extnum, phone, fax, mobile, qq, email, mac
+    extnum, phone, fax, mobile, qq, email, ip
 ]
