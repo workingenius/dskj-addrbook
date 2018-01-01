4e5f7a453e3afdd9765d7a5775454637d5a32ce6
dskj <srq@ktdatasoft.com>
2018-01-01 15:25:59 +0800

    flat: Replace auth api

--- addresslist/urls.py
+++ addresslist/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # url(r'^staffs$', views.LocaffList.as_view(), name='staff_list'),
     # url(r'^staffs/(?P<id>\w*)$', views.LocaffDetail.as_view(), name='staff_detail'),
     # url(r'^export', views.export, name='export'),
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
