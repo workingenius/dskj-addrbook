6956fbeeb208a7997c8f2fcc0361fd0e4710243f
dskj <srq@ktdatasoft.com>
2018-01-01 11:55:02 +0800

    flat: Record with null values

--- flat/models.py
+++ flat/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Record(models.Model):
-    name = models.CharField(max_length=32)
-    depart1 = models.CharField(max_length=32)
-    depart2 = models.CharField(max_length=32)
-    extnum = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
-    fax = models.CharField(max_length=128)
-    mobile = models.CharField(max_length=128)
-    qq = models.CharField(max_length=128)
-    email = models.CharField(max_length=128)
+    name = models.CharField(max_length=32, null=True)
+    depart1 = models.CharField(max_length=32, null=True)
+    depart2 = models.CharField(max_length=32, null=True)
+    extnum = models.CharField(max_length=128, null=True)
+    phone = models.CharField(max_length=128, null=True)
+    fax = models.CharField(max_length=128, null=True)
+    mobile = models.CharField(max_length=128, null=True)
+    qq = models.CharField(max_length=128, null=True)
+    email = models.CharField(max_length=128, null=True)
--- imprt.py
+++ imprt.py
@@ -4,17 +4,24 @@ import pandas as pd
 from flat.models import Record
 
 
+def fmt(x):
+    if pd.isnull(x):
+        return None
+    else:
+        return x
+
+
 df = pd.read_excel('assets/SLC3.xlsx', u'最新通讯录')
 for idx, row in df.iterrows():
     r = Record(
-        name=row[u'姓名'],
-        depart1=row[u'部门1'],
-        depart2=row[u'部门2'],
-        extnum=row[u'分机号'],
-        phone=row[u'直线'],
-        fax=row[u'传真'],
-        mobile=row[u'手机'],
-        qq=row[u'QQ'],
-        email=row[u'邮件地址']
+        name=fmt(row[u'姓名']),
+        depart1=fmt(row[u'部门1']),
+        depart2=fmt(row[u'部门2']),
+        extnum=fmt(row[u'分机号']),
+        phone=fmt(row[u'直线']),
+        fax=fmt(row[u'传真']),
+        mobile=fmt(row[u'手机']),
+        qq=fmt(row[u'QQ']),
+        email=fmt(row[u'邮件地址'])
     )
     r.save()
