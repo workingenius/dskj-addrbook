b086665963926d8e35080494ffd019ea1d4d6815
dskj <srq@ktdatasoft.com>
2019-07-10 20:59:58 +0800

    更新启动脚本

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-nohup uwsgi ./uwsgi.ini &
+#!/bin/bash
+export PATH="/home/ytjx/miniconda2/bin/:$PATH"
+. activate dskj-addrbook
+which python
+cd /home/ytjx/service
+uwsgi /home/ytjx/service/uwsgi.ini
